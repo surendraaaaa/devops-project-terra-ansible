9a878cac1d7837347cd2607db0a83675072c6f57
surendraaaaa <114964508+surendraaaaa@users.noreply.github.com>
2025-10-14 20:43:21 -0400

    Update outputs.tf

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,15 +1,14 @@
 output "cluster_name" {
-  value = aws_eks_cluster.my_eks.name
+  description = "Name of the EKS cluster"
+  value       = aws_eks_cluster.main.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.my_eks.endpoint
+  description = "Endpoint for the EKS cluster"
+  value       = aws_eks_cluster.main.endpoint
 }
 
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.my_eks.vpc_config[0].cluster_security_group_id
-}
-
-output "node_group_name" {
-  value = aws_eks_node_group.my_eks_node.node_group_name
+output "cluster_certificate_authority_data" {
+  description = "Certificate authority data for the EKS cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
 }
