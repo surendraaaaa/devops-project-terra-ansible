8544e54d4d80b056d21405f1abf5941bdc7ece48
surendraaaaa <114964508+surendraaaaa@users.noreply.github.com>
2025-10-14 20:46:58 -0400

    Update variables.tf

--- terraform/environment/dev/variables.tf
+++ terraform/environment/dev/variables.tf
@@ -2,5 +2,11 @@ variable "aws_region" {
   default = "us-east-2"
 }
 
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+}
+
 
 
