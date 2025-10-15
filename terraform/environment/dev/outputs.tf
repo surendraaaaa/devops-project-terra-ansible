cf03b54925cc5c78756683f49c4e0aa043ff3929
surendraaaaa <114964508+surendraaaaa@users.noreply.github.com>
2025-10-14 20:41:42 -0400

    Update outputs.tf

--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -1,4 +1,15 @@
-output "kubeconfig" {
-  value     = module.eks.kubeconfig
-  sensitive = true
+
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Certificate authority data for the EKS cluster"
+  value       = module.eks.cluster_certificate_authority_data
 }
