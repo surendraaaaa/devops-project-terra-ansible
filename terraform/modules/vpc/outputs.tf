a880d2e01ecc4dd5ab4974df0a3b54ac27a0b5a1
surendraaaaa <114964508+surendraaaaa@users.noreply.github.com>
2025-10-14 21:00:50 -0400

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,11 +1,21 @@
 output "vpc_id" {
-  value = aws_vpc.my_vpc.id
+  description = "ID of the VPC"
+  value       = aws_vpc.my_vpc.id
 }
 
-output "subnet_ids" {
-  value = [for subnet in aws_subnet.my_subnet : subnet.id]
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public_subnets[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private_subnets[*].id
 }
 
 output "security_group_id" {
-  value = aws_security_group.my_sg.id
+  description = "ID of the security group"
+  value       = aws_security_group.my_sg.id
 }
+
+
